b345f21d6b26cc00cef92b5605b575f4f921d0aa
”Filip <Filip.edfors@hotmail.com>
2021-10-28 20:32:38 +0200

    Creating contructor for ButtonLogic

--- src/OOPSprint3/ButtonLogic.java
+++ src/OOPSprint3/ButtonLogic.java
@@ -1,4 +1,29 @@
 package OOPSprint3;
 
-public class ButtonLogic {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class ButtonLogic extends JButton implements ActionListener {
+
+    final int finalPlacement;
+    int myDestination;
+    ArrayList<ButtonLogic> Button; // samma arraylist från Panel class
+    Panel panel;
+
+    public ButtonLogic(String name, int finalPlacement, int myDestination, ArrayList<ButtonLogic> Buttons, Panel panel) {
+        setText(name);
+        this.finalPlacement = finalPlacement;
+        this.myDestination = myDestination;
+        this.Button = Buttons;
+        this.panel = panel;
+        addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
